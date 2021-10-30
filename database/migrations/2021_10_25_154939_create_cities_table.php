4f220d47ec82ac42ad5adf30184ff462605ef238
SamehFurqan <smta55@hotmail.com>
2021-10-30 06:22:16 +0200

    custom migration for citis in egypt

--- database/migrations/2021_10_25_154939_create_cities_table.php
+++ database/migrations/2021_10_25_154939_create_cities_table.php
@@ -11,26 +11,28 @@ class CreateCitiesTable extends Migration
      *
      * @return void
      */
-    // public function up()
-    // {
-    //     Schema::create('cities', function (Blueprint $table) {
-    //         $table->id();
-    //         $table->string("name");
-    //         $table->string("name_ar")->nullable();
-    //         $table->boolean("active")->default(1);
-    //         $table->text("deactivation_notes")->nullable();
-    //         $table->decimal("delivery_fees", 8, 2)->nullable();
-    //         $table->timestamps();
-    //     });
-    // }
+    public function up()
+    {
+        \DB::unprepared( file_get_contents( "database/areas.sql" ) );
+
+        Schema::table('cities', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string("name");
+        //     $table->string("name_ar")->nullable();
+            $table->boolean("active")->default(1);
+        //     $table->text("deactivation_notes")->nullable();
+            $table->decimal("delivery_fees", 8, 2)->nullable();
+        //     $table->timestamps();
+        });
+    }
 
     // /**
     //  * Reverse the migrations.
     //  *
     //  * @return void
     //  */
-    // public function down()
-    // {
-    //     Schema::dropIfExists('cities');
-    // }
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
 }

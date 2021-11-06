adc1beb97a55abcf1e69ebbc34f56748a98794d0
SamehFurqan <smta55@hotmail.com>
2021-11-07 01:51:51 +0200

    edit config

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://api.talsystem.com'),
 
     'asset_url' => env('ASSET_URL', null),
 

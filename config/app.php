555bf037b0043bc7dd45ff8baca20020285570f8
SamehFurqan <smta55@hotmail.com>
2021-11-06 17:35:34 +0200

    remove spati permissions

--- config/app.php
+++ config/app.php
@@ -188,7 +188,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Spatie\Permission\PermissionServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class,
     ],
 

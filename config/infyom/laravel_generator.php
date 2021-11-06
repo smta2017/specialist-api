c4e64387f0ff04b5ffbfc97045a6c089c741ca0c
SamehFurqan <smta55@hotmail.com>
2021-11-06 20:52:43 +0200

    Revert "infyom swagger instalation"
    This reverts commit 04e78d709a625af2edc0f644b5fab9e795853b83.

--- config/infyom/laravel_generator.php
+++ config/infyom/laravel_generator.php
@@ -170,11 +170,11 @@ return [
 
     'add_on' => [
 
-        'swagger'       => true,
+        'swagger'       => false,
 
         'tests'         => true,
 
-        'datatables'    => true,
+        'datatables'    => false,
 
         'menu'          => [
 
